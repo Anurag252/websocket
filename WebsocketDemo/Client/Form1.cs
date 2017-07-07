a1a3744a07d2c6b11307ffd8d7c90a762a8538f3
ady8 <anurag.dubey@3ds.com>
2017-07-07 18:06:56 +0530

    test

---
+++ WebsocketDemo/Client/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TcpClient sock = new TcpClient("127.0.0.1", 1112);
+            
+            Stream s = sock.GetStream();
+            StreamWriter sw = new StreamWriter(s);
+
+            sw.Write("Hello");
+            s.Close();
+            sock.Close();
+
+        }
+    }
+}
---
+++ WebsocketDemo/WebsocketDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace WebsocketDemo
+{
+    class Program
+    {
+
+      static  Socket sock;
+        static void Main(string[] args)
+        {
+            TcpListener sck = new TcpListener(IPAddress.Any, 1112);
+            sck.Start();
+            Console.WriteLine("Server started");
+            
+          sock =   sck.AcceptSocket();
+            byte[] buffer = new byte[10000];
+             sock.Receive(buffer);
+            Console.WriteLine("found a connection");
+
+            //   System.IO.Stream s = new NetworkStream(sock);
+            // StreamReader sreader = new StreamReader(s);
+            //  string k = sreader.ReadToEnd();
+            string k = Encoding.ASCII.GetString(buffer);
+            Console.WriteLine();
+          //  s.Close();
+            sock.Close();
+            Console.Read();
+        }
+    }
+}
